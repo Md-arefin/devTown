2f4bdc3901ec8901d2229911ee1d39dcdd1bbd65
Md-arefin <ansariarefin617@gmail.com>
2023-11-02 14:34:47 +0600

    added products

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,66 @@ import './App.css'
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [filters, setFilters] = useState({
-    price: '',
-    size: '',
-    category: '',
-  });
 
   useEffect(() => {
 
     fetch('data.json')
       .then(res => res.json())
       .then(data => {
-        setFilteredProducts(data)
         setProducts(data)
       })
 
   }, []);
-  let categoryData = products?.map(product => product.category);
 
-  categoryData = ["All", ...new Set(categoryData)]
+  let categoryData = products?.map(product => product.category);
+  categoryData = ["All", ...new Set(categoryData)];
+
+  console.log(['categoryData'], categoryData);
+
+  const handleCategory = category => {
+    console.log(category);
+  }
+  // Style component
+
 
   return (
+
     <>
-      <p>Dev Town: {products.length}</p>
-      <h1>
-        {
-          categoryData
-        }
-      </h1>
+      <h1 className='text-center text-3xl font-semibold my-5'>Welcome to DevTown</h1>
+      <div className='flex flex-row gap-10'>
+
+        <div className='w-1/2 h-[450px] grid grid-cols-1 px-10 sticky top-[10px]'>
+          {
+            categoryData.map((data, i) =>
+              <h5 key={i}
+                className='my-2 border-2 rounded-lg py-1 px-5 cursor-pointer'
+                onClick={() => handleCategory(data)}
+              >
+                {data}
+              </h5>)
+          }
+        </div>
+
+        <div className='grid grid-cols-3 gap-5'>
+          {
+            products?.map((product, i) =>
+              <div key={i} className='rounded-lg w-full m-2 border-2'>
+                <img src={product.img} className='rounded-t-lg mx-auto' alt="" />
+                <h1 className='font-semibold text-center py-2 text-2xl '>{product.name}</h1>
+                <div className='py-1 pl-10'>
+                  <p><span className='text-xl'>category:</span> {product.category}</p>
+                  <p><span className='text-xl'>seller:</span> {product.seller}</p>
+                  <p><span className='text-xl'>shipping:</span> ${product.shipping}</p>
+                  <p><span className='text-xl'>stock:</span> {product.stock}</p>
+                  <p><span className='text-xl'>Price:</span> ${product.price}</p>
+                  <p><span className='text-xl'>ratings:</span> {product.ratings}</p>
+                </div>
+              </div>)
+          }
+        </div>
+      </div>
     </>
+
   )
 }
 
