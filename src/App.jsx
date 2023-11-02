a838d5e438ac74d840b383772fd2039ff44d8e97
Md-arefin <ansariarefin617@gmail.com>
2023-11-02 21:03:53 +0600

    implemented pagination

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 function App() {
   const [products, setProducts] = useState([]);
   const [productsData, setProductsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 9;
 
   useEffect(() => {
 
@@ -18,10 +20,15 @@ function App() {
 
   }, []);
 
-  let categoryData = products?.map(product => product.category);
-  categoryData = ["All", ...new Set(categoryData)];
+  const totalPages = Math.ceil(productsData.length / productsPerPage);
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const currentProducts = productsData.slice(startIndex, endIndex);
 
-  console.log(['categoryData'], categoryData);
+  const categoryData = products?.map(product => product.category);
+  const uniqueCategoryData = ["All", ...new Set(categoryData)];
+
+  // console.log(['uniqueCategoryData'], uniqueCategoryData);
 
   const handleCategory = category => {
     console.log(category);
@@ -32,7 +39,15 @@ function App() {
     } else {
       setProductsData(products)
     }
+
+    setCurrentPage(1);
   }
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+
   // Style component
 
 
@@ -44,7 +59,7 @@ function App() {
 
         <div className='w-72 h-[450px] grid grid-cols-1 px-10 sticky top-[10px]'>
           {
-            categoryData.map((data, i) =>
+            uniqueCategoryData.map((data, i) =>
               <h5 key={i}
                 className='my-2 border-2 rounded-lg py-1 px-5 cursor-pointer'
                 onClick={() => handleCategory(data)}
@@ -56,7 +71,7 @@ function App() {
 
         <div className='grid grid-cols-3 gap-5'>
           {
-            productsData?.map((product, i) =>
+            currentProducts?.map((product, i) =>
               <div key={i} className='rounded-lg w-96 m-2 pb-5 border-2'>
                 <img src={product.img} className=' rounded-t-lg mx-auto' alt="" />
                 <h1 className='font-semibold text-center py-2 text-2xl '>{product.name}</h1>
@@ -64,7 +79,6 @@ function App() {
                   <p><span className='text-xl'>category:</span> {product.category}</p>
                   <p><span className='text-xl'>seller:</span> {product.seller}</p>
                   <p><span className='text-xl'>stock:</span> {product.stock}</p>
-                  <p><span className='text-xl'>shipping:</span> ${product.shipping}</p>
                   <p><span className='text-xl'>Price:</span> ${product.price}</p>
                   <p className='flex gap-3 items-center '><span className='text-xl'>ratings:</span> {product.ratings}<Rating
                     style={{ maxWidth: 100 }}
@@ -73,9 +87,23 @@ function App() {
                   />
                   </p>
                 </div>
-              </div>)
-          }
+              </div>
+              )}
         </div>
+
+      </div>
+
+      <div className="pagination text-center my-2">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`pagination-button  ${currentPage === index + 1 ? 'active' : ''
+              }`}
+          >
+            <p className='border-2 px-5 mx-2'>{index + 1}</p>
+          </button>
+        ))}
       </div>
     </>
 
