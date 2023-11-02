f690088de83d4b3006900225d417e32990b4443a
Md-arefin <ansariarefin617@gmail.com>
2023-11-02 12:45:47 +0600

    added category

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [filters, setFilters] = useState({
+    price: '',
+    size: '',
+    category: '',
+  });
+
+  useEffect(() => {
+
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => {
+        setFilteredProducts(data)
+        setProducts(data)
+      })
+
+  }, []);
+  let categoryData = products?.map(product => product.category);
+
+  categoryData = ["All", ...new Set(categoryData)]
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Dev Town: {products.length}</p>
+      <h1>
+        {
+          categoryData
+        }
+      </h1>
     </>
   )
 }
